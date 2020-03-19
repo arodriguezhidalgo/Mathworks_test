b8f3c3c9e5b182696d74be60dafd45a833fe9740
arodh91 <arodh91@gmail.com>
2020-03-19 23:03:21 +0100

    v1.0

--- solverTest.m
+++ solverTest.m
@@ -230,24 +230,24 @@ function test_dim(testCase)
     % Then, we check some exceptional cases that should throw an error.    
     actual_out = 'error';
     % ** It should fail if we use duration data.
-    try sum(data,hours(1)); catch fun_out = 'error'; end
+    try fun_out = sum(data,hours(1)); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. Tried to use duration type for dim');
     
     % ** We should get an error if we use an unrounded float or char 'dim' 
     % value.   
-    try sum(data,'b'); catch fun_out = 'error'; end
+    try fun_out = sum(data,'b'); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. Mistake using b as a parameter for sum');
     
-    try sum(data, 1.5); catch fun_out = 'error'; end
+    try fun_out = sum(data, 1.5); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. A float dim number was used');
         
     % ** We should also verify that it does not work with a negative 'dim'
     % value.
-    try sum(data, -1); catch fun_out = 'error'; end
+    try fun_out = sum(data, -1); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. A float dim number was used');
     
     % ** In fact, it should not work if we set dim=0.
-    try sum(data, 0); catch fun_out = 'error'; end
+    try fun_out = sum(data, 0); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. A float dim number was used');
     
     % ** Here is another exceptional case, which is the term 'a' that is
@@ -267,7 +267,7 @@ function test_dim(testCase)
     verifyEqual(testCase, sum(data), data, 'DIM. Sum of a single number failed');      
     
     % ** Sum should fail if we use a complex number as dim.
-    try sum(data, 1+j); catch fun_out = 'error'; end
+    try fun_out = sum(data, 1+j); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'DIM. Sum using complex number for dim produced unexpected result');
 
 
@@ -338,45 +338,45 @@ function test_vecdim(testCase)
        
     % ** NEGATIVE elements in vecdim.
     actual_out = 'error';
-    try sum(data, [-1, 2]); catch fun_out = 'error'; end
+    try fun_out = sum(data, [-1, 2]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a NEGATIVE element in vecdim didnt throw error.');
     
     % ** ZERO element in vecdim.
-    try sum(data, [2,0]); catch fun_out = 'error'; end
+    try fun_out = sum(data, [2,0]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a ZERO element in vecdim didnt throw error.');
     
     % ** CHAR element in vecdim.
-    try sum(data, ['a',3]); catch fun_out = 'error'; end
+    try fun_out = sum(data, ['a',3]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a CHAR element in vecdim didnt throw error.');
     
     % ** FLOAT element in vecdim.
-    try sum(data, [1, 2.5]); catch fun_out = 'error'; end
+    try fun_out = sum(data, [1, 2.5]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a FLOAT element in vecdim didnt throw error.');
         
     % ** MATRIX used as vecdim.
-    try sum(data, reshape(1:4,[2,2])); catch fun_out = 'error'; end
+    try fun_out = sum(data, reshape(1:4,[2,2])); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a MATRIX in vecdim didnt throw error.');
         
     % ** Check if vecdim receives an incompatible structure. We use a
     % cell, but there are other alternatives.
-    try sum(data, {1,2}); catch fun_out = 'error'; end
+    try fun_out = sum(data, {1,2}); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using a CELL in vecdim didnt throw error.');
     
     % ** Check if vecdim contains nonunique elements.
-    try sum(data, [3,1,3]); catch fun_out = 'error'; end
+    try fun_out = sum(data, [3,1,3]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Sum using NONUNIQUE ELEMENTS in vecdim didnt throw error.');
     
     % ** Sum should fail if we use a vecdim with a complex number
-    try sum(data, [1+j, 2]); catch fun_out = 'error'; end
+    try fun_out = sum(data, [1+j, 2]); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Using complex vecdim produced unexpected results')
     
     % ** Sum should fail if vecdim contains the binarized version of a
     % vector such as [1,2], since it turns into [1,1]. The same thing
     % occurs with duration data.
-    try sum(data, logical([1,2])); catch fun_out = 'error'; end
+    try fun_out = sum(data, logical([1,2])); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Tried to binarize vecdim and sum failed')
     
-    try sum(data, hours([1,2])); catch fun_out = 'error'; end
+    try fun_out = sum(data, hours([1,2])); catch fun_out = 'error'; end
     verifyEqual(testCase, fun_out, actual_out, 'VECDIM. Tried to use a duration-like vecdim and sum failed')
 end
 
@@ -385,31 +385,60 @@ end
 % 'nanflag' tests.
 % VERIFY COMPLEX NUMBERS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+function test_nanflag(testCase)
+    % We check both nanflags.
+    data = single(ones(3));
+    data(3) = NaN;
+    
+    actual_out = single(NaN);
+    fun_out = sum(data, 'all','native');
+    verifyEqual(testCase, fun_out, actual_out, 'NAN. Flag includenan failed');
+    
+    actual_out = single(8);
+    fun_out = sum(data, 'all','native','omitnan');
+    verifyEqual(testCase, fun_out, actual_out, 'NAN. Flag omitnan failed');
+    % We also check that it provides an error when nanflag is not properly
+    % set.
+    actual_out = 'error';
+    try fun_out = sum(1:5, 2,'double','a'); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'NAN. Sum using integer nanflag didnt throw error');
+    
+end
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function args tests.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function test_wrong_input_datatype(testCase)
+function test_wrong_call(testCase)
     % We check here if the function produces an 'error' outcome when the input
     % is not numeric. We do this using a cell that contains a vector inside.
     data = {1:5};
     actual_out = 'error';
     try
-        sum(data)
+        fun_out = sum(data)
     catch
         fun_out = 'error';
-        verifyEqual(testCase, fun_out, actual_out,['Error. It provides output for incompatible input'])
+        verifyEqual(testCase, fun_out, actual_out,['CALL. It provides output for incompatible input'])
     end
 
-    % We check what happens when 'outtype' or 'nanflag' is an integer.
+    % We check what happens when 'outtype' or 'nanflag' is an integer using
+    % all the available arguments.
     actual_out = 'error';
-    try sum(1:5, 2,1,'omitnan'); catch fun_out = 'error'; end
-    verifyEqual(testCase, fun_out, actual_out, 'Sum using integer outtype didnt throw error');
+    try fun_out = sum(1:5, 2,1,'omitnan'); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'CALL. Sum using integer outtype didnt throw error');
     
-    try sum(1:5, 2,'double',0); catch fun_out = 'error'; end
-    verifyEqual(testCase, fun_out, actual_out, 'Sum using integer nanflag didnt throw error');
+    try fun_out = sum(1:5, 2,'double',0); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'CALL. Sum using integer nanflag didnt throw error');
     
     % Sum should fail if we provide more than 3 input arguments (excluding
     % the input vector/matrix).
+    try fun_out = sum(magic(3), 2,'double','omitnan',42); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'CALL. Using more than three input arguments');
+    
+    % It should fail if we provide no input argument.
+    try fun_out = sum(); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'CALL. User provided no input');
+    
+    % It should fail if we request more than one output.
+    try [a,b] = sum(magic(3)); catch fun_out = 'error'; end
+    verifyEqual(testCase, fun_out, actual_out, 'CALL. Requested more than one output');
 end
